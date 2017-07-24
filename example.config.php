3e67732159d5623a8f5ca7897da12d012afc973e
danvop <danvop@gmail.com>
2017-07-25 09:53:24 +1000

    example.config.php added

---
+++ example.config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This is example config file
+ * Please rename to config.php
+ * Config parameters for connection to database
+ */
+return [
+    'database' => [
+        'pass' => 'password',
+        'login' => 'user',
+        'host' => '10.0.0.4',
+        'dbname' => 'syslog'
+    ]
+];
